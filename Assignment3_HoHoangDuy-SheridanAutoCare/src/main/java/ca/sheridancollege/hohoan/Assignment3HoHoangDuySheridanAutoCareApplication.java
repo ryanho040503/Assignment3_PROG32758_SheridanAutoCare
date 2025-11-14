eaba17dce8b86b06ab0e57e6b9c32b45175cd066
Hồ Hoàng Duy <ryanho040503@gmail.com>
2025-11-14 12:26:31 -0500

    Initializing Working Environment

---
+++ Assignment3_HoHoangDuy-SheridanAutoCare/src/main/java/ca/sheridancollege/hohoan/Assignment3HoHoangDuySheridanAutoCareApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hohoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3HoHoangDuySheridanAutoCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3HoHoangDuySheridanAutoCareApplication.class, args);
+	}
+
+}
